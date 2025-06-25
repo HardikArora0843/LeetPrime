9cbd45d3d3e57cef9f4424ff6e058b917740db5e
HardikArora0843 <118887883+HardikArora0843@users.noreply.github.com>
2025-06-26 01:25:33 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+    baseURL: process.env.BACKEND_URL',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
